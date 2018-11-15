5d3a317665ff35ec96633740d9ee0cdedcb92137
Thomson Mathew <thomson_mat@yahoo.ca>
2018-11-15 12:53:28 -0500

    commiting .R file name

---
+++ experiment_coding.R
@@ -0,0 +1,10 @@
+world_height_inches <- c(60,64,63,70,84,80,87,56,62,68,71,72,75,76)
+hist(world_height_inches)
+mean_heights <-  mean(world_height_inches)
+stddeviation <- sd(world_height_inches)
+prop_over_7feet <- pnorm(84,mean_heights,stddeviation)
+prop_over_7feet_tail <- 1- pnorm(84,mean_heights,stddeviation)
+prop_over_7feet*100##print the result
+##testing the push function now
+
+
